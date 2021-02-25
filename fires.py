1457919925c02074a2ff7da1c9bc03a432aca46e
Igor <61599472+Mertvbli@users.noreply.github.com>
2021-02-25 10:19:14 +0700

    Add files via upload

---
+++ fires.py
@@ -0,0 +1,53 @@
+import csv
+from datetime import datetime
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+num_rows = 10_000
+
+filename = 'D://pyy/data_json/world_fires_1_day.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # Получите яркость, широту и долготу и даты
+    dates, brightnesses, lats, lons, hover_texts = [], [], [], [], []
+    row_num = 0
+    for row in reader:
+        date = datetime.strptime(row[5], '%Y-%m-%d')
+        brightness = float(row[2])
+        label = f"{date.strftime('%m/%d/%y')} - {brightness}"
+
+        dates.append(date)
+        brightnesses.append(brightness)
+        lats.append(row[0])
+        lons.append(row[1])
+        hover_texts.append(label)
+
+        row_num += 1
+        if row_num == num_rows:
+            break
+
+
+# Составить карту пожаров
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'text': hover_texts,
+    'marker': {
+        'size': [brightness / 20 for brightness in brightnesses],
+        'color': brightnesses,
+        'colorscale': 'YlOrRd',
+        'reversescale': False,
+        'colorbar': {'title': 'Brightness'},
+
+    },
+}]
+
+my_layout = Layout(title='World Fires')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_fires.html')
+
